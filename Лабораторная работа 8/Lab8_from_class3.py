1f12a3aff56a1c8a4e9c2148d971d8bd7cf42397
RGrigorii <118053826+RGrigorii00@users.noreply.github.com>
2022-11-13 21:02:41 +0500

    Add files via upload

---
+++ Лабораторная работа 8/Lab8_from_class3.py
@@ -0,0 +1,59 @@
+### LAB NUMBER 8 PEP8 ###
+
+### Human input
+name_he = input()
+height_he = int(input())
+
+
+class Students:
+
+
+	def __init__(self, idn, name, height):
+			self.idn = idn
+			st_idn = self.idn
+			self.name = name
+			st_names = self.name
+			self.height = height
+			st_heights = self.height
+
+			### Algoritm Compare
+			count = 0
+			len_0 = len(name_he)
+			len_1 = len(st_names)
+			elem_0 = str(name_he)
+			elem_1 = st_names
+			compare_return = 0
+
+			### Search maximum length
+			if len_0 > len_1:
+				max_len = len_0
+			else:
+				max_len = len_1
+
+			### Iterating over segments of word
+			for i in range(max_len):
+				try:
+					if elem_0[i] == elem_1[i]:
+						count += 1
+				except:
+					pass
+
+			compare_return = count / len_1 
+			'''
+			 len_1 потомучто если ввести символов многобольше чем 
+			 длина имени, то не работает как нужно
+			'''
+
+			### Search condition
+			if ((st_heights - 5 < height_he < st_heights + 5) and (compare_return >= 0.4)):
+				person = (st_idn, st_names, height)
+				print(person)
+
+
+###               ID --- Name --- Height
+Natasha = Students(0, "Natasha", 160)
+Natasha = Students(1, "Natasha", 160)
+Oleg    = Students(2, "Oleg", 180)
+Oleg    = Students(3, "Oleg", 183)
+Olga    = Students(4, "Olga", 158)
+Olga    = Students(5, "Olga", 170)
